6373fb30dac91d5d6ee0e1140e10a8cbeb4e03c7
Willinton Muñoz Espinal <wilintonmunoz09@gmail.com>
2022-02-25 18:34:31 -0500

    FIxed authentication method. Now use user AUTH

--- frontend/components/faq/faqList.component.tsx
+++ frontend/components/faq/faqList.component.tsx
@@ -5,24 +5,23 @@ import { Question } from '../../models/question.model';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-const token_refresher = async () => {
-  let response = await fetch(`${process.env.AUTH}`, {
-    method: 'post',
-    body: JSON.stringify({identifier: "refresh@fresh.com", password: "refresh"})
-  });
-  let responseJson = await response.json();
-  return responseJson.jwt;
-}
+
+const fetchCredential = () => fetch(`${process.env.AUTH}`, {
+  body: JSON.stringify({
+    identifier: process.env.USER,
+    password: process.env.PASSWORD
+  })
+}).then(response => response.json().then(_response => _response.jwt));
 
 const fetcher = (url: any) => fetch(url, { // Fetcher for the SWR request
   headers: new Headers({
-    'Authorization': `Bearer ${token_refresher()}` // Attach Strapi token store in environment
+    'Authorization': `Bearer ${fetchCredential()}` // Attach Strapi token store in environment
   })
 }).then(response => response.json());
 
 const FaqList: NextPage = () => {
   const router = useRouter(); // Router injection to know the actual locale of the page
-  const {locale, locales, defaultLocale} = router; // Unpack router locale information
+  const {locale} = router; // Unpack router locale information
   var {data, error} = useSWR(`${process.env.QUESTIONS_API}?locale=${locale}`, fetcher); // Fetch information from the API using long pulling
 
   function dataMapper() {
