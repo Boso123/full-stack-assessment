cba18a4e8b41156bd2bd25437a8dc5292299bac5
Willinton Muñoz Espinal <wilintonmunoz09@gmail.com>
2022-02-25 20:29:16 -0500

    Fixed some errors, resolve watherfall in authentication, And fixed token authentication

--- frontend/components/faq/faqList.component.tsx
+++ frontend/components/faq/faqList.component.tsx
@@ -5,30 +5,41 @@ import { Question } from '../../models/question.model';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+/**
+ * Renew the user token and fetch the data from the server
+ * @param url_data Url API wich provides de data
+ * @param url_auth Url API wich provides de authentication
+ * @returns `Promise<any>` Promise with the response of the data API in json Format
+ */
+const fetcher = async (url_data: any, url_auth: any) => {
+  var token = await fetch(url_auth, { // Fetch token
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({
+      identifier: process.env.USER,
+      password: process.env.PASSWORD
+    })
+  });
 
-const fetcherCredential = (url: any) => fetch(url, {
-  method: 'POST',
-  headers: new Headers({
-    'Content-Type': 'application/json'
-  }),
-  body: JSON.stringify({
-    identifier: process.env.USER,
-    password: process.env.PASSWORD
-  })
-}).then(response => response.json());
+  var _token = await token.json();
+  token = _token.jwt; // Get token from response
 
-const fetcher = (url: any, token: any) => fetch(url, { // Fetcher for the SWR request
-  headers: new Headers({
-    'Authorization': `Bearer ${token}` // Attach Strapi token store in environment
-  })
-}).then(response => response.json());
+  var result = await fetch(url_data, { // Fetcher for the SWR request
+    headers: new Headers({
+      'Authorization': `Bearer ${token}` // Attach Strapi token store in environment
+    })
+  });
+
+  return result.json()
+}
 
 const FaqList: NextPage = () => {
   const router = useRouter(); // Router injection to know the actual locale of the page
   const { locale } = router; // Unpack router locale information
-  var question: any = undefined;
-  //var { data: token } = useSWR(`${process.env.STRAPI_AUTH}`, fetcherCredential);
-  //var { data: question, error } = useSWR(token ? [`${process.env.QUESTIONS_API}?locale=${locale}`, token.jwt] : `${process.env.QUESTIONS_API}?locale=${locale}`, fetcher); // Fetch information from the API using long pulling
+  var { data: question, error } = useSWR(
+    [`${process.env.QUESTIONS_API}?locale=${locale}`, `${process.env.STRAPI_AUTH}`], fetcher); // Fetch information from the API using long pulling
 
   function dataMapper() { // Map the attributes of the fetched data
     try {
